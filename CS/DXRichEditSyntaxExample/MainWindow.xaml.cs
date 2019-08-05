f7c18c2d7a69e5372b382ac8225ebeec4f498df4
Poline Fedorova <polina.fedorova@devexpress.com>
2019-08-05 16:52:42 +0300

    update code sample

--- CS/DXRichEditSyntaxExample/MainWindow.xaml.cs
+++ CS/DXRichEditSyntaxExample/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using DevExpress.Office.Utils;
 using DevExpress.XtraRichEdit.API.Native;
 using DevExpress.XtraRichEdit.Services;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DXRichEditSyntaxExample
 {
@@ -35,65 +26,101 @@ namespace DXRichEditSyntaxExample
     public class CustomSyntaxHighlightService : ISyntaxHighlightService
     {
         readonly Document document;
-        SyntaxHighlightProperties defaultSettings = new SyntaxHighlightProperties() { ForeColor = Color.Black };
-        SyntaxHighlightProperties keywordSettings = new SyntaxHighlightProperties() { ForeColor = Color.Blue };
-        SyntaxHighlightProperties stringSettings = new SyntaxHighlightProperties() { ForeColor = Color.Green };
 
-        string[] keywords = new string[] {
-                "INSERT", "SELECT", "CREATE", "TABLE", "USE", "IDENTITY", "ON", "OFF", "NOT", "NULL", "WITH", "SET" };
+        Regex _keywords;
+        //Declare a regular expression to search text in quotes (including embedded quotes)
+        Regex _quotedString = new Regex(@"'([^']|'')*'");
+
+        //Declare a regular expression to search commented text (including multiline)
+        Regex _commentedString = new Regex(@"(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)");
 
         public CustomSyntaxHighlightService(Document document)
         {
             this.document = document;
+
+            //Declare keywords
+            string[] keywords = { "INSERT", "SELECT", "CREATE", "TABLE", "USE", "IDENTITY", "ON", "OFF", "NOT", "NULL", "WITH", "SET", "GO", "DECLARE", "EXECUTE", "NVARCHAR", "FROM", "INTO", "VALUES", "WHERE", "AND" };
+            this._keywords = new Regex(@"\b(" + string.Join("|", keywords.Select(w => Regex.Escape(w))) + @")\b");
+        }
+
+        public void Execute()
+        {
+            List<SyntaxHighlightToken> tSqltokens = ParseTokens();
+            document.ApplySyntaxHighlight(tSqltokens);
+        }
+        public void ForceExecute()
+        {
+            Execute();
         }
 
         private List<SyntaxHighlightToken> ParseTokens()
         {
             List<SyntaxHighlightToken> tokens = new List<SyntaxHighlightToken>();
             DocumentRange[] ranges = null;
-            // search for quotation marks
-            ranges = document.FindAll("'", SearchOptions.None);
-            for (int i = 0; i < ranges.Length / 2; i++)
-            {
-                tokens.Add(new SyntaxHighlightToken(ranges[i * 2].Start.ToInt(),
-                    ranges[i * 2 + 1].Start.ToInt() - ranges[i * 2].Start.ToInt() + 1, stringSettings));
-            }
-            // search for keywords
-            for (int i = 0; i < keywords.Length; i++)
-            {
-                ranges = document.FindAll(keywords[i], SearchOptions.CaseSensitive | SearchOptions.WholeWord);
 
-                for (int j = 0; j < ranges.Length; j++)
-                {
-                    if (!IsRangeInTokens(ranges[j], tokens))
-                        tokens.Add(new SyntaxHighlightToken(ranges[j].Start.ToInt(), ranges[j].Length, keywordSettings));
-                }
+            // Search for quoted strings
+            ranges = document.FindAll(_quotedString);
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                tokens.Add(CreateToken(ranges[i].Start.ToInt(), ranges[i].End.ToInt(), Color.Red));
             }
-            // order tokens by their start position
+
+            //Extract all keywords
+            ranges = document.FindAll(_keywords);
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                //Check whether tokens intersect
+                if (!IsRangeInTokens(ranges[j], tokens))
+                    tokens.Add(CreateToken(ranges[j].Start.ToInt(), ranges[j].End.ToInt(), Color.Blue));
+            }
+
+            //Find all comments
+            ranges = document.FindAll(_commentedString);
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                //Check whether tokens intersect
+                if (!IsRangeInTokens(ranges[j], tokens))
+                    tokens.Add(CreateToken(ranges[j].Start.ToInt(), ranges[j].End.ToInt(), Color.Green));
+            }
+
+            // Sort tokens by their start position
             tokens.Sort(new SyntaxHighlightTokenComparer());
-            // fill in gaps in document coverage
-            AddPlainTextTokens(tokens);
+
+            // Fill in gaps in document coverage
+            tokens = CombineWithPlainTextTokens(tokens);
             return tokens;
         }
 
-        private void AddPlainTextTokens(List<SyntaxHighlightToken> tokens)
+        //Parse the remaining text into tokens:
+        List<SyntaxHighlightToken> CombineWithPlainTextTokens(List<SyntaxHighlightToken> tokens)
         {
-            int count = tokens.Count;
-            if (count == 0)
+            List<SyntaxHighlightToken> result = new List<SyntaxHighlightToken>(tokens.Count * 2 + 1);
+            int documentStart = this.document.Range.Start.ToInt();
+            int documentEnd = this.document.Range.End.ToInt();
+            if (tokens.Count == 0)
+                result.Add(CreateToken(documentStart, documentEnd, Color.Black));
+            else
             {
-                tokens.Add(new SyntaxHighlightToken(0, document.Range.End.ToInt(), defaultSettings));
-                return;
+                SyntaxHighlightToken firstToken = tokens[0];
+                if (documentStart < firstToken.Start)
+                    result.Add(CreateToken(documentStart, firstToken.Start, Color.Black));
+                result.Add(firstToken);
+                for (int i = 1; i < tokens.Count; i++)
+                {
+                    SyntaxHighlightToken token = tokens[i];
+                    SyntaxHighlightToken prevToken = tokens[i - 1];
+                    if (prevToken.End != token.Start)
+                        result.Add(CreateToken(prevToken.End, token.Start, Color.Black));
+                    result.Add(token);
+                }
+                SyntaxHighlightToken lastToken = tokens[tokens.Count - 1];
+                if (documentEnd > lastToken.End)
+                    result.Add(CreateToken(lastToken.End, documentEnd, Color.Black));
             }
-            tokens.Insert(0, new SyntaxHighlightToken(0, tokens[0].Start, defaultSettings));
-            for (int i = 1; i < count; i++)
-            {
-                tokens.Insert(i * 2, new SyntaxHighlightToken(tokens[i * 2 - 1].End,
- tokens[i * 2].Start - tokens[i * 2 - 1].End, defaultSettings));
-            }
-            tokens.Add(new SyntaxHighlightToken(tokens[count * 2 - 1].End,
- document.Range.End.ToInt() - tokens[count * 2 - 1].End, defaultSettings));
+            return result;
         }
 
+        //Check whether tokens intersect
         private bool IsRangeInTokens(DocumentRange range, List<SyntaxHighlightToken> tokens)
         {
             return tokens.Any(t => IsIntersect(range, t));
@@ -106,24 +133,23 @@ namespace DXRichEditSyntaxExample
             int end = range.End.ToInt() - 1;
             if (end >= token.Start && end < token.End)
                 return true;
+            if (start < token.Start && end >= token.End)
+                return true;
             return false;
         }
 
-        public void ForceExecute()
+        SyntaxHighlightToken CreateToken(int start, int end, Color foreColor)
         {
-            Execute();
+            SyntaxHighlightProperties properties = new SyntaxHighlightProperties();
+            properties.ForeColor = foreColor;
+            return new SyntaxHighlightToken(start, end - start, properties);
         }
-        public void Execute()
+        public class SyntaxHighlightTokenComparer : IComparer<SyntaxHighlightToken>
         {
-            document.ApplySyntaxHighlight(ParseTokens());
-        }
-    }
-
-    public class SyntaxHighlightTokenComparer : IComparer<SyntaxHighlightToken>
-    {
-        public int Compare(SyntaxHighlightToken x, SyntaxHighlightToken y)
-        {
-            return x.Start - y.Start;
+            public int Compare(SyntaxHighlightToken x, SyntaxHighlightToken y)
+            {
+                return x.Start - y.Start;
+            }
         }
     }
 }
